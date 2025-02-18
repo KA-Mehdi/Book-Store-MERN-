cfb571dc1e7c4c3974e86647a6610a40bf373b98
mehdi <kamilmehdi449@gmail.com>
2025-02-18 13:45:23 +0100

    book store done

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb+srv://root:root@books-store-mern.jk0nn.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
